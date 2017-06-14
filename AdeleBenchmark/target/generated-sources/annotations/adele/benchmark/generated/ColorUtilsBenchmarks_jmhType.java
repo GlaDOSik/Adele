fe04cdf5c1ec7f88f708864a7132ffdc8195e93e
GlaDOSik <ludek422@seznam.cz>
2017-06-14 21:35:04 +0200

    Added gitignore

--- AdeleBenchmark/target/generated-sources/annotations/adele/benchmark/generated/ColorUtilsBenchmarks_jmhType.java
+++
@@ -1,4 +0,0 @@
-package adele.benchmark.generated;
-public class ColorUtilsBenchmarks_jmhType extends ColorUtilsBenchmarks_jmhType_B3 {
-}
-
--- AdeleBenchmark/target/generated-sources/annotations/adele/benchmark/generated/FrameBenchmarks_jmhType.java
+++
@@ -1,4 +0,0 @@
-package adele.benchmark.generated;
-public class FrameBenchmarks_jmhType extends FrameBenchmarks_jmhType_B3 {
-}
-
